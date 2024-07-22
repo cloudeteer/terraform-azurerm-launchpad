2d9900de95869720dcafc5c49209521d25126b5b
philthoennissen <pth@cloudeteer.de>
2024-07-22 15:50:29 +0200

    example updated
    Signed-off-by: philthoennissen <pth@cloudeteer.de>

--- examples/usage/main.tf
+++ examples/usage/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "example" {
 
 module "example" {
   source                = "cloudeteer/launchpad/azurerm"
-  github_pat = "" #
+  github_pat = "justRandomChars" #
   vnet_address_space    = ["192.168.0.0/24"]
   snet_address_prefixes = ["192.168.0.0/24"]
   resource_group_name   = azurerm_resource_group.example.name
