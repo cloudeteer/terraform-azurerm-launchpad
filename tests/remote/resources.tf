98da56f8c5241c8ffdf40009bcb049e9ee4866fa
phil-thoennissen <pth@cloudeteer.de>
2025-09-01 11:11:35 +0200

    terraform pre-commit fix
    Signed-off-by: phil-thoennissen <pth@cloudeteer.de>

--- tests/remote/resources.tf
+++ tests/remote/resources.tf
@@ -1,9 +1,11 @@
 variable "location" {
-  type = string
+  type        = string
+  description = "The geographic location where the resources will be deployed. This is must be a region name supported by Azure."
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "The name of the resource group in which the virtual machine should exist. Changing this forces a new resource to be created."
 }
 
 resource "random_string" "resource_group_suffix" {
@@ -18,11 +20,13 @@ resource "azurerm_resource_group" "tftest" {
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.tftest.name
+  value       = azurerm_resource_group.tftest.name
+  description = "The name of the resource group in which the virtual machine should exist. Changing this forces a new resource to be created."
 }
 
 output "resource_group_location" {
-  value = azurerm_resource_group.tftest.location
+  value       = azurerm_resource_group.tftest.location
+  description = "The geographic location where the resources will be deployed. This is must be a region name supported by Azure."
 }
 
 data "http" "init_access_ip_address" {
@@ -30,5 +34,6 @@ data "http" "init_access_ip_address" {
 }
 
 output "init_access_ip_address" {
-  value = trimspace(data.http.init_access_ip_address.response_body)
+  value       = trimspace(data.http.init_access_ip_address.response_body)
+  description = "The IP Address of your current public IP in order to access the new created resources. For more information please go here https://github.com/cloudeteer/terraform-azurerm-launchpad/blob/main/INSTALL.md "
 }
